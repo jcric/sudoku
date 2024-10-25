82d94e85212b64c7836984585b88955d6a9084a6
Jonas Christian Richter <jori4013@myrdal.it.uu.se>
2024-10-25 22:28:10 +0200

    intro files

---
+++ SudokuSolver.hs
@@ -0,0 +1,16 @@
+
+-- \/\/\/ DO NOT MODIFY THE FOLLOWING LINES \/\/\/
+module SudokuSolver(Board, Solutions(..), author, nickname, numSolutions) where
+import Sudoku(Board, Solutions(..))
+-- /\/\/\ DO NOT MODIFY THE PRECEDING LINES /\/\/\
+{- (Remember to provide a brief (about 100-500 words) description of
+your implementation.)
+-}
+author :: String
+author = undefined -- replace `undefined' with your first and last name
+nickname :: String
+nickname = undefined -- replace `undefined' with a nickname for your solver
+{- (Remember to provide a complete function specification.)
+-}
+numSolutions :: Board -> Solutions
+numSolutions = undefined -- remove `= undefined' and define your function here
