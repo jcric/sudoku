3a407668d87bb05e996c4b5164cf57ec382e4bb0
Jonas Christian Richter <jori4013@myrdal.it.uu.se>
2024-10-28 23:01:47 +0100

    uploaded correct files

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,15 +1,17 @@
--- \/\/\/ DO NOT MODIFY THE FOLLOWING LINES \/\/\/
-module SudokuSolver(Board, Solutions(..), author, nickname, numSolutions) where
-import Sudoku(Board, Solutions(..))
--- /\/\/\ DO NOT MODIFY THE PRECEDING LINES /\/\/\
-{- (Remember to provide a brief (about 100-500 words) description of
-your implementation.)
+-- \/\/\/ DO NOT MODIFY THIS FILE \/\/\/
+module Sudoku(Board, Solutions(..)) where
+{- The three possible numbers of solutions: 0, 1, or more than 1.
 -}
-author :: String
-author = undefined -- replace `undefined' with your first and last name
-nickname :: String
-nickname = undefined -- replace `undefined' with a nickname for your solver
-{- (Remember to provide a complete function specification.)
+data Solutions = NoSolution | UniqueSolution | MultipleSolutions
+{- A Sudoku board
+The board is represented as a list of rows,
+where each row is a list of ints,
+where 0 represents an empty cell.
+INVARIANTS:
+The length of the Board is the square of a positive integer.
+The board is square, so the length of a Board is the same as the length of each
+of its elements.
+The board contains only numbers between 0 and the length of the Board
+(inclusive).
 -}
-numSolutions :: Board -> Solutions
-numSolutions = undefined -- remove `= undefined' and define your function here
+type Board = [[Int]]
