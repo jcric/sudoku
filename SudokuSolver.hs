5f82acd0c92cc06d3af326fbbddb1f612e4c2d82
Jonas Christian Richter <jori4013@myrdal.it.uu.se>
2024-10-29 18:04:27 +0100

    adding sources

--- SudokuSolver.hs
+++ SudokuSolver.hs
@@ -3,14 +3,45 @@
 module SudokuSolver(Board, Solutions(..), author, nickname, numSolutions) where
 import Sudoku(Board, Solutions(..))
 -- /\/\/\ DO NOT MODIFY THE PRECEDING LINES /\/\/\
-{- (Remember to provide a brief (about 100-500 words) description of
-your implementation.)
+
+{-
+SOURCES:
+For my solution, I have decided to implement a solver based on the one presented by Graham Hutton on his Youtube channel of the same
+name under his course Advanced Functional Programming. He in turn has based his solver on Professor Richard Bird´s solution, which
+was originally implemented in Pearl. Both sources are linked below:
+
+Graham Hutton´s Youtube channel:
+https://www.youtube.com/@haskellhutt
+
+Richard Bird´s paper:
+https://www.cs.tufts.edu/~nr/cs257/archive/richard-bird/sudoku.pdf
+
+EXPLANATION OF SOLUTION:
+
 -}
+
 author :: String
 author = "Jonas Richter"
 nickname :: String
 nickname = "jcric"
-{- (Remember to provide a complete function specification.)
+
+
+{-
+numSolutions
+
+Takes a Board and returns the number of ways that it can be completed into a solved Sudoku: 0, 1, or several.
+
+RETURNS:
+PARAMETERS:
+SIDE EFFECTS: None. Also, no input or output operations from e.g. the console, file system or network are performed.
+EXAMPLES:
+
+
 -}
-numSolutions :: Board -> Solutions
-numSolutions = undefined -- remove `= undefined' and define your function here
+
+--numSolutions :: Board -> Solutions
+--numSolutions = undefined -- remove `= undefined' and define your function here
+
+
+numSolutions :: Board -> IO()
+numSolutions b = print b
